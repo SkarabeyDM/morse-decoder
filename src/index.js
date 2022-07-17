2ab63c04010859147f1d3275c8d7140b029a9acf
Jamal Muslimov <mechanoid0415@gmail.com>
2022-07-17 15:16:32 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -36,10 +36,24 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
 
 function decode(expr) {
-    // write your solution here
+    const space = "**********"
+    return expr.match(/.{1,10}/g).map(letter => letter === space ? " " : MORSE_TABLE[binToMorse(letter)]).join("")
 }
+
+function binToMorse(str) {
+    return str.match(/.{1,2}/g).map(symb => {
+            switch (symb) {
+                case "00": return ""
+                case "10": return "."
+                case "11": return "-"
+            }
+        }).join("")
+}
+
+console.log(decode(expr))
 
 module.exports = {
     decode
